7e9030fdfff5776d079eddd3fd5e92ebe87e984b
Juan Foncuberta Calbeto <juan@juanfoncuberta.com>
2018-09-19 22:25:51 +0200

    Adding stylesheet to housesview and function call to nav houses -> characters

--- src/components/sections/houses/view.js
+++ src/components/sections/houses/view.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 import { View,Text, Button } from 'react-native'
 import { Actions } from 'react-native-router-flux'
+import  styles from './styles'
 
 
 export default class Houses extends Component{
 
+    constructor(props){
+        super(props)
+        this.navToCharactersView = this.navToCharactersView.bind(this)
+    }
+    navToCharactersView(){
+        Actions.characters()
+    }
     render(){
         return(
-            <View style={{flex: 1, backgroundColor: 'blue', alignItems: 'center',  justifyContent: 'center' }}>
+            <View style={styles.container}>
                 <Text style={{color: 'white' }}>Houses</Text>
-                <Button title={'Nav to characters'} color={'yellow'} onPress={()=> Actions.characters()} />
+
+                <Button 
+                    title={'Nav to characters'} 
+                    color={'yellow'} 
+                    onPress={this.navToCharactersView} />
             </View>
         )
     }
