27319c158b3ef844e9c0575135093c3924d953db
Juan Foncuberta Calbeto <juan@juanfoncuberta.com>
2018-09-26 00:11:28 +0200

    Connecting characters to redux

--- src/components/sections/characters/view.js
+++ src/components/sections/characters/view.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import styles from './styles'
+import { connect } from 'react-redux'
 
+class Characters extends Component {
 
-export default class Characters extends Component {
+    componentDidMount(){
+        this.props.fetchHouseCharacters()
+    }
     render(){
         return(
-            <View style={{flex: 1, backgroundColor: 'lime', alignItems: 'center',  justifyContent: 'center' }}>
-                <Text style={{color: 'white' }}>Characters</Text>
+            <View style={styles.container}>
+
             </View>
         )
     }
 }
+
+const mapStateToProps  = (state) => {
+    return {
+        isFetching: state.characters.isFetching
+    }
+}
+
+function mapDispatachTopProps(dispatch, props){
+    return {
+        fetchHouseCharacters: () => {
+   
+        }
+    }
+}
+
+export default connect(mapStateToProps,mapDispatachTopProps)(Characters)
