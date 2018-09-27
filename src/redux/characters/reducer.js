27b61421fbabc3d3f9d3d369977b911c76521fad
Juan Foncuberta Calbeto <juan@juanfoncuberta.com>
2018-09-27 20:23:33 +0200

    Creating characters reducer, defining types

---
+++ src/redux/characters/reducer.js
@@ -0,0 +1,30 @@
+import * as types from './types'
+
+const initialState = {
+    list: [],
+    isFetching: false,
+    item: null
+}
+
+export default function reducer( state = initialState, action = {} ){
+
+    switch(action.type){
+        case types.CHARACTER_SET_FETCHING:
+            return {
+                ...state,
+                isFetching: action.value
+            }
+        case types.CHARACTERS_SET_ITEM:
+            return {
+                ...state,
+                item: action.value
+            }
+        case types.CHARACTERS_UPDATE_LIST:
+            return {
+                ...state,
+                list: action.value
+            }
+        default:
+            return action.value
+    }
+} 
